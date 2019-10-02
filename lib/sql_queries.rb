7b813419e48d89c699daaff400c8b2bf8f115e1e
Juan Carlo Banayo <johncban@gmail.com>
2019-10-02 13:57:45 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,6 +8,9 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "Write your SQL query Here"
+  SELECT projects.title, projects.funding_goal
+  FROM projects
+  ORDER BY title ASC;
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name

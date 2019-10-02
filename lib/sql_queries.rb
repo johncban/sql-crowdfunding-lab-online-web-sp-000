a58aa1296d7addcf9166a6d4fd3f93e6e338b312
Juan Carlo Banayo <johncban@gmail.com>
2019-10-02 15:42:02 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "Write your SQL query Here"
-  SELECT projects.title, projects.funding_goal
+  SELECT projects.title, SUM(projects.funding_goal)
   FROM projects
   ORDER BY title ASC;
 end
